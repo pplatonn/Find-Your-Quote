01667204d932d1c4e79b852f0911c61cf7d0cd21
Polina Platonova <polinaplatonova122@gmail.com>
2025-04-08 22:30:04 +0300

    basic algorithm added

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -2,21 +2,61 @@ import React from 'react'
 import { useState } from 'react'
 import "./Quotes.css"
 
-// get the api
-// create a variable for quote number
-// fetch api with quote num
-// show text of api
-
 function Quotes() {
 
-    const [quoteNum, setQuoteNum] = useState(1)
+    const [id, setId] = useState()
+    const [quote, setQuote] = useState([])
+    const [err, setErr] = useState("")
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        fetch(`https://dummyjson.com/quotes/${id}`)
+            .then(res => res.json())
+            .then((data) => {
+                setQuote(data);
+                setErr("");
+                console.log(data);
+            })
+            .catch((err) => {
+                setErr(err);
+                setQuote(null);
+                console.log(err)
+            }
+            )
+
+    }
+
+    const handleInputChange = (event) => {
+
+        const value = event.target.value;
+
+        if (!isNaN(value)) {
+            setId(value);
+            setErr('');
+        } else {
+            setErr('enter a NUMBER, please');
+            console.log(err)
+            return
+        }
+    }
 
     return (
         <div className='container'>
-            <form>
-                <h1>fill</h1>
+            <form method='post' onSubmit={handleSubmit}>
+                <h1>fill the quote number:</h1>
+                <input onChange={handleInputChange} placeholder='enter the quote number' />
+                <button type='button' onClick={handleSubmit}>find</button>
             </form>
+            <div className='quoteContainer'>
+                {quote && (
+                    <div>
+                        <blockquote>"{quote.quote}"</blockquote>
+                        <span>- {quote.author}</span>
+                    </div>
+                )}
+                {err && <div id="error">{err}</div>}
+            </div>
         </div>
     )
 }
